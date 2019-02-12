f9353383ba3066ac4acfd32bf7545e136f9be0df
KMM <i001962@gmail.com>
2019-02-12 15:41:03 -0800

    first

---
+++ memphis-catalog/data/config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  siteTitle: 'Memphis.io', // Site title.
+  siteTitleAlt: 'Smart business docs', // Alternative site title for SEO.
+  siteLogo: '/icons/icon-512x512.png', // Logo used for SEO and manifest.
+  siteUrl: 'https://memphis-catalog-v1.netlify.com', // Domain of your website without pathPrefix.
+  pathPrefix: '/', // Prefixes all links. For cases when deployed to example.github.io/gatsby-starter-business/.
+  siteDescription: 'Smart business docs ', // Website description used for RSS feeds/meta description tag.
+  siteRss: '/rss.xml',
+  siteFBAppID: '', // FB Application ID for using app insights
+  googleTagManagerID: '', // GTM tracking ID.
+  disqusShortname: 'gatsby-business-starter', // Disqus shortname.
+  userName: 'Kevin McDonald',
+  userTwitter: 'i001962',
+  userLocation: 'Woodside, CA',
+  userDescription: '',
+  copyright: 'Copyright © Memphis.io 201. All Rights Reserved.', // Copyright string for the footer of the website and RSS feed.
+  themeColor: '#00d1b2', // Used for setting manifest and progress theme colors.
+  backgroundColor: '#ffffff', // Used for setting manifest background color.
+}
